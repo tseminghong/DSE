ea79ee3094fc3399f86a93569d1a44646e4c118c
tseminghong <tseminghong5@gmail.com>
2025-10-27 22:45:13 +0800

    add support for math and history

--- dse-react/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header';
-export { default as GlassCard } from './GlassCard';
-export { default as DownloadButton } from './DownloadButton';
-export { default as FloatingElements } from './FloatingElements';
-export { default as Layout } from './Layout';
